1480075f25e75c8c324b9a306e6922c64e5b0b89
ArmaanChaand <armaan.chaand17@gmail.com>
2025-12-25 18:26:05 +0530

    redirected 'library/*' to 'library.devredbox.in'

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/library',
+        destination: 'https://library.devredbox.in',
+        permanent: true
+      },
+      {
+        source: '/library/:path*',
+        destination: 'https://library.devredbox.in',
+        permanent: true
+      },
+    ]
+  },
 };
 
 export default nextConfig
